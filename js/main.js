faca0a1196e5080ac0b96ed7320080332bd76235
Prof3ssorSt3v3 <griffis@algonquincollege.com>
2025-01-28 12:54:59 -0500

    end of first class

--- js/main.js
+++ js/main.js
@@ -6,6 +6,29 @@ function init() {
   setUpWorker();
   addListeners();
   pageSpecific();
+  getBreeds();
+}
+
+function getBreeds() {
+  let url = dogBaseURL + 'breeds/list/all';
+  fetch(url)
+    .then((resp) => {
+      if (!resp.ok) throw new Error('No breeds available');
+      return resp.json(); // .text()  .blob()
+    })
+    .then((data) => {
+      let select = document.getElementById('breeds');
+      Object.keys(data.message).forEach((br) => {
+        let opt = document.createElement('option');
+        opt.textContent = br;
+        opt.value = br;
+        select.append(opt);
+      });
+    })
+    .catch((err) => {
+      console.warn(err.message);
+      //TODO: display error for user
+    });
 }
 
 function setUpWorker() {
@@ -16,11 +39,85 @@ function setUpWorker() {
 
 function addListeners() {
   //for all pages
+  //service worker messages
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker.ready.then((reg) => {
       reg.active.addEventListener('message', gotMessage);
     });
   }
+  //form submit
+  document.querySelector('form').addEventListener('submit', doSearch);
+  //adopt buttons
+  document.querySelector('main').addEventListener('click', doAdopt);
+}
+
+function doAdopt(ev) {
+  let target = ev.target;
+  if (target.localName === 'button' && target.className === 'btnAdopt') {
+    //only if user clicked a button with the className btnAdopt
+    let card = target.closest('.card');
+    let img = card.querySelector('img');
+    let src = img.src;
+    let name = img.alt;
+    let msg = {
+      action: 'adopt',
+      dog: { src, name },
+    };
+    sendMessage(msg);
+  }
+}
+
+function doSearch(ev) {
+  ev.preventDefault(); //stop the page reloading
+  let breed = document.getElementById('breeds').value;
+  let url = dogBaseURL;
+  if (breed == '') {
+    //any breed
+    url += 'breeds/image/random/3';
+  } else {
+    //specific breed
+    url += `breed/${breed}/images/random/3`;
+  }
+  let nameURL = nameBaseURL + '?results=3'; //url for dog names
+  Promise.all([fetch(url), fetch(nameURL)])
+    .then((respArr) => {
+      if (!respArr[0].ok) throw new Error('No dogs to be found');
+      if (!respArr[1].ok) throw new Error('No names to be found');
+
+      return Promise.all([respArr[0].json(), respArr[1].json()]);
+    })
+    .then((dataArr) => {
+      //build the html
+      let images = dataArr[0];
+      let names = dataArr[1];
+      console.log(images);
+      console.log(names);
+      document.querySelector('main').innerHTML = '';
+      for (let i = 0; i < images.message.length; i++) {
+        let div = document.createElement('div');
+        div.className = 'card';
+        let p1 = document.createElement('p');
+        div.append(p1);
+        let img = document.createElement('img');
+        img.src = images.message[i];
+        img.alt = names.results[i]?.name.first;
+        p1.append(img);
+        let p2 = document.createElement('p');
+        div.append(p2);
+        p2.textContent = names.results[i]?.name.first;
+        let p3 = document.createElement('p');
+        div.append(p3);
+        let btn = document.createElement('button');
+        btn.className = 'btnAdopt';
+        btn.innerHTML = '<span class="material-symbols-outlined">heart_plus</span> Adopt Now!';
+        p3.append(btn);
+        document.querySelector('main').append(div);
+      }
+    })
+    .catch((err) => {
+      console.warn(err.message);
+      //TODO: display the error for the user
+    });
 }
 
 function gotMessage(ev) {
--- sw.js
+++ sw.js
@@ -5,13 +5,41 @@ const adoptCache = 'adoptedDogs_' + version;
 
 const appFiles = ['/', '/index.html', '/adopt.html', '/css/main.css', '/js/main.js'];
 
-self.addEventListener('install', (ev) => {});
-self.addEventListener('activate', (ev) => {});
+self.addEventListener('install', (ev) => {}); //Homework
+self.addEventListener('activate', (ev) => {}); //Homework
 self.addEventListener('fetch', (ev) => {});
-self.addEventListener('message', (ev) => {});
+self.addEventListener('message', (ev) => {
+  if ('action' in ev.data && ev.data.action == 'adopt') {
+    adoptADog(ev.data.dog);
+  }
+});
+
+function adoptADog(dog) {
+  let str = JSON.stringify(dog);
+  let filename = crypto.randomUUID() + '.json';
+  let file = new File([str], filename, { type: 'application/json' });
+  let resp = new Response(file, { status: 200, headers: { 'content-type': 'application/json' } });
+  let request = new Request(`/${filename}`);
+
+  caches
+    .open(adoptCache)
+    .then((cache) => {
+      cache.put(request, resp);
+    })
+    .then(() => {
+      console.log('saved the adoption');
+      sendMessage({ message: 'Dog successfully adopted', filename: filename });
+    });
+}
 
 function sendMessage(msg, client) {
   //client is falsey means send to all
+
+  clients.matchAll().then((clientList) => {
+    for (let client of clientList) {
+      client.postMessage(msg);
+    }
+  });
 }
 
 function cacheOnly(ev) {
